b53271783d31ac84811a2e2d93e517c36e62df8f
Jyothi Kaku <jyothikaku229@gmail.com>
2025-09-25 21:10:38 +0530

    cors setup

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.CLIENT_URL || 'http://localhost:5173', 
     credentials: true,               
   }));
   
